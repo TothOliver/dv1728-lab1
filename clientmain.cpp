91e5edcac63587b07013e06fbf5bf91d87b4cecc
Oliver Toth <90266732+TothOliver@users.noreply.github.com>
2025-09-14 16:27:55 +0200

    UDP Binary without timeout

--- clientmain.cpp
+++ clientmain.cpp
@@ -1,10 +1,16 @@
-//Try push
-//Try commit?
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
 /* You will to add includes here */
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdint.h>
+#include <protocol.h>
 
 // Enable if you want debugging to be printed, see examble below.
 // Alternative, pass CFLAGS=-DDEBUG to make, make CFLAGS=-DDEBUG
@@ -127,7 +133,7 @@ int main(int argc, char *argv[]){
     
   /* Do magic */
   int port=atoi(Destport);
-  if (port < 1000 or port >65535) {
+  if (port < 1 or port >65535) {
     printf("Error: Port is out of server scope.\n");
     if ( port > 65535 ) {
       printf("Error: Port is not a valid UDP or TCP port.\n");
@@ -136,8 +142,140 @@ int main(int argc, char *argv[]){
   }
 #ifdef DEBUG 
   printf("Protocol: %s Host %s, port = %d and path = %s.\n",protocol, Desthost,port, Destpath);
-#endif
 
 
+  if(strcmp(protocol, "UDP") != 0 || strcmp(Destpath, "binary") != 0)
+  {
+    fprintf(stderr, "Error: Not udp or binary supported protocol\n");
+    return EXIT_FAILURE;
+  }
+
+  struct addrinfo hints, *results;
+  int sockfd;
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_DGRAM;
+
+  int status = getaddrinfo(Desthost, Destport, &hints, &results);
+  if(status != 0 || results == NULL)
+  {
+    fprintf(stderr, "ERROR: Ressolve Issue");
+    return EXIT_FAILURE;
+  }
+
+  char ipstr[INET6_ADDRSTRLEN]; 
+  for(struct addrinfo *p = results; p != NULL; p = p->ai_next) {
+    void *addr;
+
+    if(p->ai_family == AF_INET){
+      struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
+      addr = &(ipv4->sin_addr);
+    }
+    else if(p->ai_family == AF_INET6){
+      struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
+      addr = &(ipv6->sin6_addr);
+    }
+    else {
+      fprintf(stderr, "Error: Not a valid IP-version\n");
+      return EXIT_FAILURE;
+    }
+
+    inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
+
+    sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+    if(sockfd == -1) {
+      fprintf(stderr, "ERROR: socket failed\n");
+      return EXIT_FAILURE;
+    }
+
+    calcMessage cmsg = {0};
+    cmsg.type = htons(22);
+    cmsg.message = htons(0);
+    cmsg.protocol = htons(17);
+    cmsg.major_version = htons(1);
+    cmsg.minor_version = htons(1);
+
+    ssize_t sent = sendto(sockfd, &cmsg, sizeof(cmsg), 0, results->ai_addr, results->ai_addrlen);
+    if(sent != sizeof(cmsg)){
+      freeaddrinfo(results);
+      close(sockfd);
+      fprintf(stderr, "ERROR: sendto failed\n");
+      return EXIT_FAILURE;
+    }
+  }
+
+  while(true){
+      char buf[1500];
+      struct sockaddr_storage addr;
+      socklen_t addr_len = sizeof(addr);
+
+      ssize_t byte_size = recvfrom(sockfd, buf, sizeof(buf), 0, (struct sockaddr*)&addr, &addr_len);
+      if(byte_size <= 0)
+      {
+        freeaddrinfo(results);
+        close(sockfd);
+        break;
+      }
+
+      if(byte_size == sizeof(calcMessage)){ //12 bytes
+        calcMessage respons;
+        memcpy(&respons, buf, sizeof(respons));
+
+        uint16_t type = ntohs(respons.type);
+        uint32_t message = ntohl(respons.message);
+        uint16_t protocol = ntohs(respons.protocol);
+        uint16_t majorv= ntohs(respons.major_version);
+        uint16_t minorv = ntohs(respons.minor_version);
+
+        printf("Got calcMessage: type=%u, message=%u, proto=%u, major=%u, minor=%u\n",
+              type, message, protocol, majorv, minorv);
+
+        if(message == 1){
+          printf("Server reply: OK\n");
+          return EXIT_SUCCESS;
+        }       
+        if(message == 2){
+          printf("Server reply: NOT OK\n");
+          return EXIT_FAILURE;
+        }       
+      }
+      else if(byte_size == sizeof(calcProtocol)){ //26 bytes
+        calcProtocol respons;
+        memcpy(&respons, buf, sizeof(respons));
+
+        uint32_t arith = ntohl(respons.arith);
+        int32_t inValue1 = ntohl(respons.inValue1);
+        int32_t inValue2 = ntohl(respons.inValue2);
+        int32_t inResult = 0;
+        
+        if(arith == 1)
+          inResult = inValue1 + inValue2;
+        else if(arith == 2)
+          inResult = inValue1 - inValue2;
+        else if(arith == 3)
+          inResult= inValue1 * inValue2;
+        else if(arith == 4)
+          inResult= inValue1 / inValue2;
+
+        respons.type = htons(2);
+        respons.inResult = htonl(inResult);
+
+        ssize_t sent = sendto(sockfd, &respons, sizeof(respons), 0, results->ai_addr, results->ai_addrlen);
+        if(sent != sizeof(respons)){
+          freeaddrinfo(results);
+          close(sockfd);
+          fprintf(stderr, "ERROR: sendto failed\n");
+          return EXIT_FAILURE;
+        }
+        printf("Respons Size:%lu\n", sizeof(respons));
+      }
+      else{
+        printf("Byte_size:%lu\n", byte_size);
+        fprintf(stderr, "ERROR: wrong size or incorrect protocol\n");
+        return EXIT_FAILURE;
+      }
+  }
   
+#endif
 }
